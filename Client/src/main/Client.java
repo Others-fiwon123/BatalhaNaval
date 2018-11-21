6390599f2b8a8a3822a2b5402484723bd414f19f
Marcos Paulo Campos <marcospcamposfilho@gmail.com>
2018-11-21 21:49:42 -0200

    Pequenos ajustes, padronização de pastas e teste server-client

--- Client/src/main/Client.java
+++ Client/src/main/Client.java
@@ -1,5 +1,13 @@
 package main;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -11,7 +19,23 @@ import manager.ScreenManager;
 
 public class Client extends Application{
 	
-	public static void  main(String args[]) {
+	public static void  main(String args[]) throws UnknownHostException, IOException {
+		
+		// Teste de comunicação com o server
+		int numero, temp;
+		Scanner entrada = new Scanner(System.in);
+		Socket socket = new Socket("localhost", 7777);
+		
+		Scanner scanner = new Scanner(socket.getInputStream());
+		System.out.println("Entre com um valor: ");
+		numero = entrada.nextInt();
+		
+		PrintStream p = new PrintStream(socket.getOutputStream());
+		p.println(numero);
+		
+		temp = scanner.nextInt();
+		System.out.println(temp);
+		
 		launch(args);
 	}
 
